d40c1a7f2abe9666c91bcf21313973d18e6e582b
Emil <emil.bertilsson91@gmail.com>
2012-04-19 16:59:36 +0200

    Camera

--- code/Pacman/Camera.cpp
+++ code/Pacman/Camera.cpp
@@ -3,9 +3,31 @@
 
 Camera::Camera()
 {
+	vEyePt		= D3DXVECTOR3	( 0,  0, 0 );
+	vLookAtPt	= D3DXVECTOR3	( 0, 0, 0 );
+	vUp			= D3DXVECTOR3	( 0, 1, 0 );
+	vDistance	= D3DXVECTOR3	( 0, -7, 15 );
+	vDesiredPt	= D3DXVECTOR3	( 0,  10, -20 );
+	
+	fRot = 0;
+	mView		= new D3DXMATRIX();
+	mProj		= new D3DXMATRIX();
+
+	D3DXMatrixLookAtLH( mView, &vEyePt, &vLookAtPt, &vUp );
+	D3DXMatrixPerspectiveLH(mProj,	(float)D3DX_PI * .6f,
+									(float)(SCREENWIDTH / _SCREENHEIGHT), 1.0f, 3000.0f);
 }
 
+void Camera::update(D3DXVECTOR3 pos, double dt)
+{
+	vEyePt.z -= 0.05f;
+
+	//vEyePt = pos / 2;
+	D3DXMatrixLookAtLH( mView, &vEyePt, &vLookAtPt, &vUp );
+}
 
 Camera::~Camera()
 {
+	SAFE_DELETE(mView);
+	SAFE_DELETE(mProj);
 }
--- code/Pacman/Camera.h
+++ code/Pacman/Camera.h
@@ -1,8 +1,26 @@
 #pragma once
+#include "stdafx.h"
+
 class Camera
 {
+private:
+	D3DXMATRIX		*mView,		//view-matrix
+					*mProj;				
+
+	D3DXVECTOR3     vEyePt,     //eye point
+					vDesiredPt,	//wanted point
+                    vLookAtPt,  //look-at target
+                    vUp,		//up-vector
+					vDistance;	//distance to player
+	float fRot;
+
 public:
 	Camera();
 	~Camera();
-};
 
+	void update		(D3DXVECTOR3 pos, double dt);	//update camera
+
+	const D3DXMATRIX		*getViewMatrix(){ return mView; }
+	const D3DXMATRIX		*getProjMatrix(){ return mProj; }
+
+};
